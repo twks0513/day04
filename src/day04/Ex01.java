706641826eee21d1a81561681d87acc8b3231bbf
Eunhyurck <Ddoo.b.b>
2022-05-18 21:36:34 +0900

    day04

--- src/day04/Ex01.java
+++ src/day04/Ex01.java
@@ -61,56 +61,7 @@ public class Ex01 {
 //			break;
 //		}
 		//-------------------------------------
-		
-//		while(true) {
-//			int date;
-//			String day;
-//			System.out.print("날짜 입력 : ");
-//			date = scan.nextInt();
-//			
-//			if(date % 7 == 1) {
-//				day = "월";
-//			}else if(date % 7 == 2) {
-//				day = "화";
-//			}else if(date % 7 == 3) {
-//				day = "수";
-//			}else if(date % 7 == 4) {
-//				day = "목";
-//			}else if(date % 7 == 5) {
-//				day = "금";
-//			}else if(date % 7 == 6) {
-//				day = "토";
-//			}else {
-//				day = "일";
-//			}
-//			
-//			switch(date%7) {
-//			case 1 :
-//				System.out.println(date+" "+day);
-//				break;
-//			case 2:
-//				System.out.println(date+" "+day);
-//				break;
-//			case 3:
-//				System.out.println(date+" "+day);
-//				break;
-//			case 4:
-//				System.out.println(date+" "+day);
-//				break;
-//			case 5:
-//				System.out.println(date+" "+day);
-//				break;
-//			case 6:
-//				System.out.println(date+" "+day);
-//				break;
-//			case 0:
-//				System.out.println(date+" "+day);
-//				break;
-//				
-//			}
-//	}
-		
-		//-------------------------------------
+
 		
 		
 		int num = 10, su = 0;
